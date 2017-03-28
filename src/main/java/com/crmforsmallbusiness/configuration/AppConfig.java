c67706a90d4c456e323d59bd5ae721789dddb2c7
Андрей <dan1sh@i.ua>
2017-03-28 21:11:07 +0300

    Create primitive DAO

---
+++ src/main/java/com/crmforsmallbusiness/configuration/AppConfig.java
@@ -0,0 +1,29 @@
+package com.crmforsmallbusiness.configuration;
+
+/**
+ * Created by Андрей on 28.03.2017.
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.crmforsmallbusiness.configuration")
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
